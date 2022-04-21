1b1da64f6b864c5da2c9f9c39bda24c1fba3b858
olivier <o.gozlan@ajc-ingenierie.fr>
2022-04-21 12:01:45 +0200

    ajout daoPersonne dans Context

--- pokemon/src/main/java/pokemon/entity/Type.java
+++ pokemon/src/main/java/pokemon/entity/Type.java
@@ -1,5 +1,5 @@
 package pokemon.entity;
 
 public enum Type {
-Feu,Eau,Plante,Electrik,Glace,Combat,Poison,Psy,Insecte,Spectre,Roche,Sol,Normal, Vol, Dragon;
+Feu,Eau,Plante,Electrik,Glace,Combat,Poison,Psy,Insecte,Spectre,Roche,Sol,Normal,Vol,Dragon;
 }
--- pokemon/src/main/java/pokemon/util/Context.java
+++ pokemon/src/main/java/pokemon/util/Context.java
@@ -5,12 +5,19 @@ import javax.persistence.Persistence;
 
 import pokemon.dao.DaoArene;
 import pokemon.dao.DaoAreneJpaImpl;
+import pokemon.dao.DaoPersonne;
+import pokemon.dao.DaoPersonneJpaImpl;
 
 public class Context {
 
 	private static EntityManagerFactory emf = null;
 
 	private static DaoArene daoArene = new DaoAreneJpaImpl();
+	private static DaoPersonne daoPersonne = new DaoPersonneJpaImpl();
+
+	public static DaoPersonne getDaoPersonne() {
+		return daoPersonne;
+	}
 
 	public static DaoArene getDaoArene() {
 		return daoArene;
