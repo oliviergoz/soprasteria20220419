97f65b5bbc18c5010d606e3b30781cc57f945076
olivier <o.gozlan@ajc-ingenierie.fr>
2022-05-03 11:07:28 +0200

    js

---
+++ web/js/monJs.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+
+//alert("Hello !");
+//console.log("Log ...");
+//console.debug("Debug ...");
+//let retour=mafonction('olivier');
+//console.log('le retour '+retour);
+//let fonction=function(){
+//	console.log('fonction variable');
+//}
+//
+//fonction();
+//console.log(mafonction());
+//console.log(mafonction);
+//
+//function mafonction(param){
+//	console.log('fonction '+param);
+//	return 'retour';
+//}
+
+
+console.log('Log');
+console.debug('Debug');
+console.warn('Warn');
+console.error('Error');
+
+console.log('5'==5);
+console.log('5'===5);
+//
+//function demoCallBack(param, fonction) {
+//	fonction(param);
+//}
+//
+//demoCallBack('olivier', console.log);
+//
+//demoCallBack(123, function(param) {
+//	console.debug(param);
+//});
+
