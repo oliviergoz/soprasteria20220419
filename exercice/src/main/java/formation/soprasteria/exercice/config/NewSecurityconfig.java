bb704995bffcf668c70f01671532592bc3d5ec7f
olivier <o.gozlan@ajc-ingenierie.fr>
2022-05-25 09:43:35 +0200

    sans le warning sur WebSecurityConfigurerAdapter

--- exercice/src/main/java/formation/soprasteria/exercice/config/NewSecurityconfig.java
+++ exercice/src/main/java/formation/soprasteria/exercice/config/NewSecurityconfig.java
@@ -1,49 +1,32 @@
 package formation.soprasteria.exercice.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+@Configuration
+public class NewSecurityconfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		// @formatter:off
 		http.antMatcher("/api/**")
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and()
 				.csrf().ignoringAntMatchers("/api/**")
 				.and()
-				.authorizeRequests()
+				.authorizeHttpRequests()
 					.antMatchers(HttpMethod.POST, "/api/auth/inscription").permitAll()
 					.antMatchers("/api/**").authenticated()
 				.and()
-				.httpBasic()	
-			.and()
-			.antMatcher("/**")
-				.authorizeRequests()
-					.antMatchers("/", "/home", "/inscription").permitAll()
-					.anyRequest().authenticated()
-				.and()
-				.formLogin();
+				.httpBasic();
 		// @formatter:on
-
-	}
-
-	@Autowired
-	private UserDetailsService userDetailsService;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
+		return http.build();
 	}
 
 	@Bean
